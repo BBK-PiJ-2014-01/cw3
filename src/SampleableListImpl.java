b113ce448e59846dd9369f072384cf4057d328f3
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-23 21:08:39 +0000

    Initial commit

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -1,37 +1,7 @@
 /**
  * Created by PierreM on 05/12/2014.
  */
-public class SampleableListImpl implements SampleableList {
-
-    @Override
-      public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public ReturnObject add(int index, Object item) {
-        return null;
-    }
-
-    @Override
-    public ReturnObject add(Object item) {
-        return null;
-    }
-
-    @Override
-    public ReturnObject get(int index) {
-        return null;
-    }
-
-    @Override
-    public ReturnObject remove(int index) {
-        return null;
-    }
+public class SampleableListImpl extends LinkedList implements SampleableList {
 
     @Override
     public SampleableList sample() {
