b0e85d391e2130b35882be8128f4df3c3c3a529b
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-26 23:35:38 +0000

    TestSampleableList - all tests included

--- src/TestSampleableList.java
+++ src/TestSampleableList.java
@@ -20,7 +20,9 @@ public class TestSampleableList {
      */
     @Test
     public void tests_sampleableListInterfaceImplementation() {
+        // checking the sampleable list is empty, if the original list is empty
         assertTrue(list.sample().isEmpty());
+        // checking the sampleable list is correct (order of elements and value), if the original list is not empty
         list.add("Item1");
         list.add("Item2");
         list.add("Item3");
@@ -28,10 +30,13 @@ public class TestSampleableList {
         list.add("Item5");
         list.add("Item6");
         SampleableList newList = list.sample();
-        assertEquals(3,newList.size());
+        assertEquals(6,list.size());
+        assertEquals(3, newList.size());
         assertEquals(list.get(0).getReturnValue(),newList.get(0).getReturnValue());
+        assertEquals("Item1",newList.get(0).getReturnValue());
         assertEquals(list.get(2).getReturnValue(),newList.get(1).getReturnValue());
+        assertEquals("Item3",newList.get(1).getReturnValue());
         assertEquals(list.get(4).getReturnValue(),newList.get(2).getReturnValue());
+        assertEquals("Item5",newList.get(2).getReturnValue());
     }
-
 }
