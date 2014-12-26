b63c2cb15cf45b94050032e8972d5365bd52a124
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-26 23:12:16 +0000

    TestFunctionalList - all tests included

--- src/TestFunctionalList.java
+++ src/TestFunctionalList.java
@@ -8,12 +8,10 @@ import static org.junit.Assert.*;
 public class TestFunctionalList {
 
     FunctionalList list;
-    FunctionalList emptyList;
 
     @Before
     public void buildUp() {
-        list = new FunctionalLinkedList();
-        emptyList = list;
+        list = new FunctionalArrayList(2);
     }
 
     /*
@@ -44,29 +42,28 @@ public class TestFunctionalList {
     @Test
     public void tests_functionalListInterfaceImplementation_restMethod() {
         // checking it returns an empty list if the original list is empty or has only one element
-        assertEquals(emptyList,list.rest());
+        assertTrue(list.rest().isEmpty());
         list.add("String1");
-        assertEquals(emptyList,list.rest());
+        assertTrue(list.rest().isEmpty());
         // checking it returns a list without the first element of the original list
-        FunctionalList list1 = list;
         list.add("String2");
-        System.out.println(list.size());
-        assertEquals(list1,list.rest());
-        System.out.println(list.size());
+        FunctionalList list1 = list.rest();
+        assertEquals(list.get(1).getReturnValue(),list1.get(0).getReturnValue());
+        assertEquals("String2",list1.get(0).getReturnValue());
         // checking the order of the elements in both lists is unchanged
         list.add("String3");
-        System.out.println(list.size());
         list.add(0,"String4");
         list.add(2,"String5");
-        FunctionalList list2 = list;
-        System.out.println(list2.size());
-        list2.remove(0);
-        assertEquals(list2, list.rest());
+        FunctionalList list2 = list.rest();
+        assertEquals(5,list.size());
+        assertEquals(4,list2.size());
         assertEquals(list.get(1).getReturnValue(), list2.get(0).getReturnValue());
-        assertEquals("String5",list2.get(0).getError());
+        assertEquals("String1",list2.get(0).getReturnValue());
         assertEquals(list.get(2).getReturnValue(),list2.get(1).getReturnValue());
+        assertEquals("String5",list2.get(1).getReturnValue());
+        assertEquals(list.get(3).getReturnValue(),list2.get(2).getReturnValue());
+        assertEquals("String2",list2.get(2).getReturnValue());
         assertEquals(list.get(4).getReturnValue(),list2.get(3).getReturnValue());
-        assertEquals(list.get(5).getReturnValue(),list2.get(4).getReturnValue());
-
+        assertEquals("String3",list2.get(3).getReturnValue());
     }
 }
